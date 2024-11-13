0b9fd1f3f8e85968ed2ced0e0d20eff6394ff8fc
arun6488 <161091363+arun6488@users.noreply.github.com>
2024-11-13 00:31:16 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_hOdrnqFmh', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '47mvqe6djl1ur4uke2865a17r1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'N. Virginia' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
