21cec13244db1e6fbb9ea258d3af5a3f94242bd9
arun6488 <161091363+arun6488@users.noreply.github.com>
2024-11-14 16:32:41 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_hOdrnqFmh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '47mvqe6djl1ur4uke2865a17r1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_ucjbKC2F5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3mkk7a94uqjpho6c2urjr25388', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://boadvp5y4h.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
