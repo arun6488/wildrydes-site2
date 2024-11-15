3c36dc806093bb331aa71b90a7d6b1629475b7d8
arun6488 <161091363+arun6488@users.noreply.github.com>
2024-11-15 22:15:48 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_373eL6ipQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1tcnfr63srqfu52sl22068foth', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_yFSlsXS6b', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3vcpo33o6k378jjposqp1ri7fv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
